d40c19927615654427f7c82e84974e5bb1349cf4
Hei34 <76108392+Hei34@users.noreply.github.com>
2025-03-10 12:56:36 +0100

    wip

--- cwiczenia2.sql
+++ cwiczenia2.sql
@@ -14,8 +14,27 @@ WHERE department_id IN (20, 50)
 ORDER BY last_name;
 
 CREATE VIEW widok2 AS
-SELECT hire_date, last_name, salary
+SELECT hire_date, last_name, &podajkol
 FROM employees
 WHERE MANAGER_ID IS NOT NULL
   AND EXTRACT(YEAR FROM hire_date) = 2005
-ORDER BY salary;
+ORDER BY podajkol;
+
+CREATE VIEW widok3 AS 
+SELECT first_name || ' ' || last_name AS full_name, salary, phone_number
+FROM employees
+WHERE last_name LIKE '__e%'
+  AND first_name LIKE '%' || &userinput || '%'
+ORDER BY 1 DESC, 2 ASC;
+
+CREATE VIEW widok4 AS SELECT 
+    first_name || ' ' || last_name AS full_name, 
+    ROUND(MONTHS_BETWEEN(SYSDATE, hire_date)) AS months_worked, 
+    CASE 
+		WHEN months_worked < 150 THEN salary * 1.1
+		WHEN months_worked BETWEEN 150 AND 200 THEN salary * 1.2
+		WHEN months_worked > 200 THEN salary * 1.3
+        ELSE salary * 1
+    END AS wysokość_dodatku
+FROM employees ORDER BY months_worked;
+
